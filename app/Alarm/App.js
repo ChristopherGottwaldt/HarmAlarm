718a69daff07017b46c58b5373c1ee1bd31711de
Christopher Gottwaldt <christophergottwaldt@gmail.com>
2023-11-11 20:45:30 -0600

    Initial creation

--- app/Alarm/App.js
+++ app/Alarm/App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text> Sussy baka sussy baka sussy baka</Text>
       <StatusBar style="auto" />
     </View>
   );
