ee11bdf37a0e3be98f2632cf91a53ae1696b606d
sshen231111 <sheny@uwplatt.edu>
2023-11-12 22:36:06 -0600

    test code for 4-by-4 keypad, OLED display on Seeeduino Lotus Grove beginner's kit, (eventual) final arduino code, and keypad LCD calculator

---
+++ arduino/HelloKeypad/HelloKeypad.ino
@@ -0,0 +1,64 @@
+/* @file HelloKeypad.pde
+|| @version 1.0
+|| @author Alexander Brevig
+|| @contact alexanderbrevig@gmail.com
+||
+|| @description
+|| | Demonstrates the simplest use of the matrix Keypad library.
+|| #
+*/
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+
+char keys[ROWS][COLS] = {
+
+  {'1','2','3','A'},
+
+  {'4','5','6','B'},
+
+  {'7','8','9','C'},
+
+  {'*','0','#','D'}
+
+};
+
+//byte rowPins[ROWS]  = { 0, 8, 2, 3 };// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+//byte colPins[COLS] = { 4, 5, 6, 7 }; // Connect keypad COL0, COL1 and COL2 to these Arduino  pins.
+//byte rowPins[ROWS]  = { 2, 3, 4, 5 };// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+//byte colPins[COLS] = { 6, 7, 8, 9 }; // Connect keypad COL0, COL1 and COL2 to these Arduino  pins.
+
+byte rowPins[ROWS]  = { 9, 8, 7, 6 };// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte colPins[COLS] = { 5, 4, 3, 2 }; // Connect keypad COL0, COL1 and COL2 to these Arduino  pins.
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+//int a;
+//int b;
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+    
+  }
+
+//  for (int x = 0; x < 4; x++){
+//      for (int y = 0; y < 4; y++){
+//           int a = digitalRead(x);
+//           int b = digitalRead(y+4);
+//           Serial.print(a);
+//           Serial.print(":");
+//           Serial.print(b);
+//           Serial.print(" ");
+//      }  
+//      Serial.println();
+//      delay(100);
+//    }
+}
---
+++ arduino/harm_alarm_arduino/harm_alarm_arduino.ino
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // High speed I2C
+
+// U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //Low spped I2C
+
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.clearBuffer();                   // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+  u8g2.drawStr(0,30,"Hello World!");    // write something to the internal memory
+  u8g2.sendBuffer();                    // transfer internal memory to the display
+  delay(1000);  
+  u8g2.clearBuffer();
+  u8g2.drawCircle(20, 25, 10, U8G2_DRAW_ALL);
+  u8g2.sendBuffer();                    // transfer internal memory to the display
+  delay(1000);
+}
